accdf3080a23e30bf33647c9657b80214dd74712
maximilian-b <maximilian.p.baehnisch@campus.tu-berlin.de>
2021-04-25 19:56:55 +0200

    parser extension + extracted naive and recursive from main

--- Exercise 1/solver.cpp
+++ Exercise 1/solver.cpp
@@ -18,6 +18,13 @@ bool is_connected(vector<vector<int>> adjacent_matrix, int index_1, int index_2)
 vector<unsigned int> find_p3_not_clique(vector<vector<int>> adjacent_matrix);
 tuple<vector<vector<int>>, bool> recursive_approach(vector<vector<int>> adjacent_matrix, int k);
 
+
+void print_2D_vector(vector<vector<int>> v);
+tuple<vector<vector<int>>,int> recursive_aproach_main(vector<vector<int>> adjacent_matrix);
+tuple<vector<vector<int>>,int> naive_aproach_main(vector<vector<int>> adjacent_matrix, int vertex_count);
+vector<vector<int>> translate_indices(vector<vector<int>> v);
+
+
 int main(int argc, char **argv) {
 	string input_line;
 	int i = 1;
@@ -33,7 +40,7 @@ int main(int argc, char **argv) {
 			continue;
 		}
 		std::vector<int> parsed_line = parse_input_adjacents(input_line);
-		if (!parsed_line.empty()) {
+		if (!parsed_line.empty() && parsed_line.size() == 3) {
 			adjacent_lines.push_back(parsed_line);
 		}
 	}
@@ -48,20 +55,16 @@ int main(int argc, char **argv) {
 		}
 		cout << endl;
 	}
-	double entry_to_calculate = (double)(pow(((double) vertex_count - 1.),2.) + vertex_count - 1.) / 2.;
-	unsigned int combination_count = (unsigned int) pow(2., entry_to_calculate);
-	//cout << combination_count << endl;
-	std::vector<vector<int>> target_vector;
-	for (int i = 0; i < vertex_count; i++) {
-		for (int j = i+1; j < vertex_count; j++) {
-			target_vector.push_back({i,j});
-		}
+	cout << "--------------" << endl;
+	tuple<vector<vector<int>>,int> result = recursive_aproach_main(adjacent_matrix);
+	//tuple<vector<vector<int>>,int> result = naive_aproach_main(adjacent_matrix, vertex_count);
+	
+	print_2D_vector(translate_indices(get<0>(result)));
+	cout << "cost: " << get<1>(result) << endl;
+	return 0;
+}
 
-	}
-
-	//for (int i = 0; i < target_vector.size(); i++) {
-	//	cout << target_vector.at(i).at(0) << "-" << target_vector.at(i).at(1) << endl;
-	//}
+tuple<vector<vector<int>>,int> recursive_aproach_main(vector<vector<int>> adjacent_matrix) {
 	tuple<vector<vector<int>>, bool> result;
 	int k = 0;
 	while (true) {
@@ -75,15 +78,30 @@ int main(int argc, char **argv) {
 
 	cout << "result k: " << k << endl;
 	vector<vector<int>> v = get<0>(result);
-	for (unsigned int i = 0; i < v.size(); i++) {
+	/*for (unsigned int i = 0; i < v.size(); i++) {
 		for (unsigned int j = 0; j < v.at(i).size(); j++) {
 			cout << v.at(i).at(j) +1 << " ";
 		}
 		cout << endl;
-	}
+	}*/
+	vector<vector<int>> changed_edges = v;
+	int cost = k;
+	tuple<vector<vector<int>>, int> end_result(changed_edges, cost);
+	return end_result;
+}
 
-	cout << "--------------" << endl;
-	/*int cost = 100000;
+tuple<vector<vector<int>>,int> naive_aproach_main(vector<vector<int>> adjacent_matrix, int vertex_count) {
+	double entry_to_calculate = (double)(pow(((double) vertex_count - 1.),2.) + vertex_count - 1.) / 2.;
+	unsigned int combination_count = (unsigned int) pow(2., entry_to_calculate);
+	//cout << combination_count << endl;
+	vector<vector<int>> target_vector;
+	for (int i = 0; i < vertex_count; i++) {
+		for (int j = i+1; j < vertex_count; j++) {
+			target_vector.push_back({i,j});
+		}
+
+	}
+	int cost = 100000;
 	unsigned int best_combination = 0;
 	unsigned int snd_combination = 0;
 	int snd_cost = 10000;
@@ -110,20 +128,23 @@ int main(int argc, char **argv) {
 
 	}
 	
-	for (int x = 0; x < vertex_count; x++) {
+	/*for (int x = 0; x < vertex_count; x++) {
 		for (int y = 0; y < vertex_count; y++) {
 			cout << adjacent_matrix_2.at(x).at(y) << " ";
 		}
 		cout << endl;
-	}
+	}*/
 	
 	cout << "minimal cost: " << cost << endl;
 	cout << "2nd cost: " << snd_cost << endl;
 	cout << "best combination: " << best_combination << endl;
 	cout << "2nd combination: " << snd_combination << endl;
-	cout << "clusters detected: " << cluster_counter << endl;*/
-	return 0;
+	cout << "clusters detected: " << cluster_counter << endl;
+	vector<vector<int>> changed_edges = {};
+	tuple<vector<vector<int>>, int> result(changed_edges, cost);
+	return result;
 }
+
 
 int parse_input_vertex_count(string input_line) {
 	if (input_line.empty()) {
@@ -140,10 +161,16 @@ std::vector<int> parse_input_adjacents(string input_line) {
 	std::vector<int> edge_tokens;
 	int token;
 	size_t position = 0;
+	size_t comment_position = input_line.find("#");
+	if (comment_position != string::npos) {
+		input_line = input_line.substr(0, comment_position);
+	}
 	while (position != string::npos) {
 		position = input_line.find(" ");
-		token = stoi(input_line.substr(0, position));
-		edge_tokens.push_back(token);
+		if (!input_line.substr(0, position).empty()) {
+			token = stoi(input_line.substr(0, position));
+			edge_tokens.push_back(token);
+		}
 		input_line.erase(0, position + 1);
 	}
 	return edge_tokens;
@@ -312,4 +339,27 @@ bool is_connected(vector<vector<int>> adjacent_matrix, int index_1, int index_2)
 		return true;
 	}
 	return false;
+}
+
+
+void print_2D_vector(vector<vector<int>> v) {
+	for (unsigned int i = 0; i < v.size(); i++) {
+		for (unsigned int j = 0; j < v.at(i).size(); j++) {
+			cout << v.at(i).at(j);
+			if (j < v.at(i).size()- 1) {
+				cout << " ";
+			}
+		}
+		cout << endl;
+	}
+	return;
+}
+
+vector<vector<int>> translate_indices(vector<vector<int>> v) {
+	for (unsigned int i = 0; i < v.size(); i++) {
+		for (unsigned int j = 0; j < v.at(i).size(); j++) {
+			v.at(i).at(j) = v.at(i).at(j) + 1;
+		}
+	}
+	return v;	
 }
